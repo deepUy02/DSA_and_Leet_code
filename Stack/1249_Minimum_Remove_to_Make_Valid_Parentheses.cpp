bc14b9e2ebd51592bfb254f308c7f11099123c58
Deepak yadav <135634621+deepUy02@users.noreply.github.com>
2024-04-07 01:09:22 +0530

    Update 1249_Minimum_Remove_to_Make_Valid_Parentheses.cpp

--- Stack/1249_Minimum_Remove_to_Make_Valid_Parentheses.cpp
+++ Stack/1249_Minimum_Remove_to_Make_Valid_Parentheses.cpp
@@ -1,3 +1,4 @@
+Approack-1 //Using Stack
 string minRemoveToMakeValid(string s) {
         stack<int> st;
         unordered_set<int>invalid;
@@ -29,4 +30,43 @@ string minRemoveToMakeValid(string s) {
             }
         }
         return ans;
+    }
+
+Approacj-2 //counting open and close brackets
+string minRemoveToMakeValid(string s) {
+        string ans="";
+        int openBrac = 0;
+
+        for(int i = 0;i< s.length();i++){
+            if(s[i] >= 'a' && s[i] <= 'z'){
+                ans.push_back(s[i]);
+            }
+            else if(s[i] == '('){
+                openBrac++;
+                ans.push_back(s[i]);
+            }
+            else if(openBrac > 0){
+                openBrac--;
+                ans.push_back(s[i]);
+            }
+        }
+
+        string final_ans = "";
+
+        int close_brac = 0;
+        for(int i = ans.length()-1 ;i >= 0 ;i--){
+            if(ans[i] >= 'a' && ans[i] <= 'z'){
+                final_ans.push_back(ans[i]);
+            }
+            else if(ans[i] == ')'){
+                close_brac++;
+                final_ans.push_back(ans[i]);
+            }
+            else if(close_brac > 0){
+                close_brac--;
+                final_ans.push_back(ans[i]);
+            }
+        }
+        reverse(begin(final_ans),end(final_ans));
+        return final_ans;
     }
